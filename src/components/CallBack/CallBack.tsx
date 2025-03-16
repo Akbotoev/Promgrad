37ab097f3c2a92e8d1f897739233a5e0aec0dc38
doni21122005 <saparbekovdoni@gmail.com>
2025-03-17 00:17:34 +0600

    footer

--- src/components/CallBack/CallBack.tsx
+++ src/components/CallBack/CallBack.tsx
@@ -1,5 +1,54 @@
 import styles from "./Styles.module.scss";
 
 export const CallBack = () => {
-  return <div></div>;
+  return (
+    <div className={styles.CallBack}>
+      <div className="container">
+        <div className={styles.content}>
+          <h2 className={styles.title}>Cвязаться с нами</h2>
+          <div className={styles.send_message}>
+            <h1 className={styles.title_send_message}>
+              Оставьте заявку – спроектируем{" "}
+              <span>вашу идею в реальность!</span>
+            </h1>
+            <form className={styles.inputs}>
+              <input
+                placeholder="ФИО/Имя компании"
+                className={styles.input}
+                type="text"
+                name="name"
+              />
+              <input
+                className={styles.input}
+                type="tel"
+                // name="phone"
+                placeholder="Номер телефона*"
+              />
+              <input
+                placeholder="Эл. адрес"
+                className={styles.input}
+                type="email"
+                // name="email"
+              />
+              <textarea
+                placeholder="Сообщение"
+                className={styles.textarea}
+                name="message"
+              ></textarea>
+              <label className={styles.label_checkbox}>
+                <input
+                  type="checkbox"
+                  className={styles.checkbox}
+                  // name="agree"
+                />
+                <span className={styles.customCheckbox}></span>
+                Согласен с условиями политики конфиденциальности
+              </label>
+              <button className={styles.button}>Отправить</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./Styles.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_left}>
+        <h1>ЛОГО</h1>
+        <div className={styles.display_footer}>
+          <div className={styles.links}>
+            <nav>
+              <a href="#">О компании</a>
+              <a href="#">Портфолио</a>
+              <a href="#">Контакты</a>
+            </nav>
+            <div className={styles.socials_links}>
+              <a href="#">Whatsapp</a>
+              <a href="#">Telegram</a>
+              <a href="#">Instagram</a>
+            </div>
+          </div>
+          <div className={styles.contact_footer}>
+            <div className={styles.contacts}>
+              <h3>Bimkg</h3>
+              <h4>+996 503 608 468</h4>
+              <h4>Улица Токтогула, дом 109</h4>
+              <h4>bimkg@gmail.com</h4>
+            </div>
+            <div className={styles.contacts}>
+              <h3>Promgrad</h3>
+              <h4>+996 703 608 468</h4>
+              <h4>Проспект Жибек Жолу, дом 555</h4>
+              <h4>promgrad@gmail.com</h4>
+            </div>
+            <div className={styles.contacts}>
+              <h3>Archi Vibe</h3>
+              <h4>+996 703 608 468</h4>
+              <h4>Проспект Эркиндик, дом 57</h4>
+              <h4>archivibe@gmail.com</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer_map}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4729.4067975671005!2d74.60048516486872!3d42.87518992249723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb7dcbdacf87b%3A0xfc7d686ab988f348!2z0J_Qu9C-0YnQsNC00Ywg0JDQu9CwLdCi0L7Qvi4!5e0!3m2!1sru!2skg!4v1742142562262!5m2!1sru!2skg"
+          style={{
+            border: "0",
+            width: "100%",
+            height: "100%",
+          }}
+          allowFullScreen // ✅ с большой буквы и без значения
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade" // ✅ camelCase
+        ></iframe>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
