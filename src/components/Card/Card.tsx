fb923765e02fd2796ef71344563e2d35c0afa2af
Beka <151364801+Akbotoev@users.noreply.github.com>
2025-03-28 14:24:24 +0600

    envl

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -28,25 +28,42 @@ const Card = () => {
   const [quantity, setQuantity] = useState<number>(8);
 
   async function getData() {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/product/product/`
-    );
-    if (brend === "Все") {
-      setData(data);
-    } else {
-      setData(data.filter((el: ICard) => el.category_name.includes(brend)));
+    try {
+      const { data } = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/product/product/`
+      );
+      console.log("Данные получены:", data);
+  
+      if (brend === "Все") {
+        setData(data);
+      } else {
+        setData(data.filter((el: ICard) => el.category_name.includes(brend)));
+      }
+    } catch (error: any) {
+      console.error("Ошибка при запросе данных:", error.response?.data || error.message);
     }
   }
-  async function getBrends() {
-    const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/product/categories/`
-    );
+  
+  
+  
+ async function getBrends() {
+  try {
+    const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/product/categories/`);
+    console.log("Категории получены:", data);
     setBrends(data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Ошибка при запросе категорий:", error.response?.data || error.message);
+    } else {
+      console.error("Ошибка при запросе категорий:", error);
+    }
   }
+}
 
   useEffect(() => {
     getData();
     getBrends();
+  
     const updateQuantity = () => {
       const width = window.innerWidth;
       if (width > 1280) {
@@ -59,14 +76,15 @@ const Card = () => {
         setQuantity(3);
       }
     };
-
+  
     updateQuantity();
     window.addEventListener("resize", updateQuantity);
-
+  
     return () => {
       window.removeEventListener("resize", updateQuantity);
     };
   }, [brend]);
+  
 
   return (
     <div id="portfolio" className={scss.Card}>
